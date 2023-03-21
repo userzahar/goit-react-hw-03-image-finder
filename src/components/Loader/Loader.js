6aba85e73d201ab03d4863f1b5f41d55a5aca0f8
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-21 21:01:45 +0200

    add LoaderStyled

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,19 +1,18 @@
-import { ThreeDots } from  'react-loader-spinner';
+import { Triangle } from 'react-loader-spinner';
 import { Div } from './Loader.styled';
 
 export function Loader() {
-    return (
-        <Div>
-            <ThreeDots 
-                height="80" 
-                width="80" 
-                radius="9"
-                color="#3f51b5" 
-                ariaLabel="three-dots-loading"
-                wrapperStyle={{}}
-                wrapperClassName=""
-                visible={true}
-            />
-        </Div>
-    );
-};
+  return (
+    <Div>
+      <Triangle
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="triangle-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
+      />
+    </Div>
+  );
+}
